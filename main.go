dd8a512e7c38e20e2cf23d2d575f83c26b30a295
shaopeng Yang <shaoyang@ebay.com>
2022-03-22 13:02:04 +0800

    update

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+/*
+ 输入任意类型，转换成int返回
+ */
+func typeSwitch(x interface{}) int {
+	switch t := x.(type) {
+	case int:
+		return t
+	case float64:
+		return int(math.Ceil(t))
+	}
+	return 0
+}
 
 func main() {
-	fmt.Println("example!")
+	num := 56.7890
+	num2 := typeSwitch(num)
+	fmt.Println(num2)
 }
